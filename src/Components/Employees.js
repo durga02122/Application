3f17fe267ae94f3dbc440ec08aefaae800295ee3
durga durga <kalyanidurga292@gmail.com>
2022-09-10 17:42:26 -0400

    Employees

---
+++ src/Components/Employees.js
@@ -0,0 +1,19 @@
+const Employees = [
+    {
+        id :"1",
+        Name :"Monoj De",
+        Age : "30"
+    },
+    {
+        id :"2",
+        Name :"Virat Kohli",
+        Age : "35"
+    },
+    {
+        id :"3",
+        Name :"Rogit Sharma",
+        Age : "38"
+    }
+
+]
+export default Employees;
